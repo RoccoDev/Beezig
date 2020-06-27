113a46b615094ac7c66625283e9f9b791297b34d
RoccoDev <roccodev.business@gmail.com>
2020-06-27 22:57:48 +0200

    reports: add info/error messages

--- src/main/java/eu/beezig/core/command/commands/ReportCommand.java
+++ src/main/java/eu/beezig/core/command/commands/ReportCommand.java
@@ -1,12 +1,18 @@
 package eu.beezig.core.command.commands;
 
+import eu.beezig.core.Beezig;
 import eu.beezig.core.command.Command;
 import eu.beezig.core.report.ReportReason;
 import eu.beezig.core.server.ServerHive;
+import eu.beezig.core.util.Color;
+import eu.beezig.core.util.text.Message;
+import eu.beezig.core.util.text.StringUtils;
 
+import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class ReportCommand implements Command {
     private static final Pattern REPORT_CMD_REGEX = Pattern.compile("(.+?)(?=(?<!,)\\s) (.+)$");
@@ -34,9 +40,13 @@ public class ReportCommand implements Command {
                 String[] reasonInputs = gReasons.split("\\s");
                 EnumSet<ReportReason> reasons = ReportReason.getReasons(reasonInputs);
                 if(reasons.size() == 0) {
+                    Message.error(Message.translate("error.report.reason"));
                     return true;
                 }
-
+                String pDisplay = Color.accent() + StringUtils.localizedJoin(Arrays.asList(players)) + Color.primary();
+                String rDisplay = Color.accent() + StringUtils.localizedJoin(reasons.stream().map(ReportReason::getDisplay)
+                        .collect(Collectors.toList())) + Color.primary();
+                Message.info(Beezig.api().translate("msg.report.submit", pDisplay, rDisplay));
             }
         }
         sendUsage("/report [player1(, player2, player3)] [reason]");
