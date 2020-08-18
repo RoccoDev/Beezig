8b6c8674c9c1a65b7d07208c96cd0ab183ee25f1
RoccoDev <roccodev.business@gmail.com>
2020-08-18 22:31:02 +0200

    reports: working report submission

--- src/main/java/eu/beezig/core/command/CommandManager.java
+++ src/main/java/eu/beezig/core/command/CommandManager.java
@@ -45,6 +45,8 @@ public class CommandManager {
         commandExecutors.add(new BUpdateCommand());
         commandExecutors.add(new BlockstatsCommand());
         commandExecutors.add(new RecordsOverlayCommand());
+        commandExecutors.add(new ReportCommand());
+        commandExecutors.add(new ReportsCommand());
     }
 
     @EventHandler
